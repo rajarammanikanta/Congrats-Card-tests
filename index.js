e73ec6f72751cd09641bae651ddf0822763b4f53
Rajaram manikanta <rajarammmanikantamani@gmail.com>
2023-01-26 19:13:14 +0000

    Congrats Card tests Solution

---
+++ index.js
@@ -0,0 +1,19 @@
+const element = (
+  <div class="bg-container1">
+    <h1>Congrautulations</h1>
+    <div class="bg-container2">
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/congrats-card-profile-img.png"
+        class="photo"
+      ></img>
+      <h1>Kiran V</h1>
+      <p>Vishnu Institute of COmputer Education and Technology Bhimavaram</p>
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/congrats-card-watch-img.png"
+        class="watch"
+      ></img>
+    </div>
+  </div>
+);
+
+ReactDOM.render(element, document.getElementById("root"));
